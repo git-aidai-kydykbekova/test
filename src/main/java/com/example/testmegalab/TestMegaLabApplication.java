e79421ac9f1a4e6324478f6367a700e6c7a5160d
git-aidai-kydykbekova <aidai.kydykbekova@alatoo.edu.kg>
2024-09-20 09:57:44 +0600

    first commit

---
+++ src/main/java/com/example/testmegalab/TestMegaLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmegalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMegaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMegaLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testmegalab/TestMegaLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testmegalab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMegaLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
